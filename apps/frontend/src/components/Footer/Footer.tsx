471bac7e897fbc2db863dc99027ae58053f06e33
thomas <tamighi@hotmail.com>
2023-07-04 13:29:55 +0200

    add(frontend): Basic footer

--- apps/frontend/src/components/Footer/Footer.tsx
+++ apps/frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,20 @@
+import "./Footer.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="Footer">
+      <a target="_blank" href="https://github.com/tamighi" className="FooterLink">
+        Github
+      </a>
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/thomas-amighi-635722196"
+        className="FooterLink"
+      >
+        Linkedin
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
