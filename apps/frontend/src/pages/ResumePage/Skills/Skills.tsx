4db2cbc2c5ce8f6fa093b874d0cf7a75f634f0e8
thomas <tamighi@hotmail.com>
2023-07-05 15:36:13 +0200

    style(frontend): Skills better styles

--- apps/frontend/src/pages/ResumePage/Skills/Skills.tsx
+++ apps/frontend/src/pages/ResumePage/Skills/Skills.tsx
@@ -1,16 +1,31 @@
+import "./Skills.css";
+
 const Skills = () => {
   return (
     <div className="ResumeSection">
       <h2 className="ResumeTitle">Skills</h2>
-      <div className="ResumeContent">
-        <ul>
-          <li>React</li>
-          <li>Nestjs</li>
-          <li>Typescript</li>
-          <li>C++</li>
-        </ul>
+      <div className="ResumeContent SkillListsContainer">
+          <ul>
+            <h4>Web dev</h4>
+            <li>React</li>
+            <li>Nestjs</li>
+            <li>Typescript</li>
+            <li>Typeorm</li>
+          </ul>
+          <ul>
+            <h4>Dev Ops</h4>
+            <li>Nginx</li>
+            <li>Webpack</li>
+            <li>Git</li>
+            <li>Docker</li>
+          </ul>
+          <ul>
+            <h4>Software development</h4>
+            <li>C++</li>
+            <li>C</li>
+          </ul>
+        </div>
       </div>
-    </div>
   );
 };
 
