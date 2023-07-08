5079f0410087eb5fb1114144eafe4652167b05c7
tamighi <tamighi@hotmail.com>
2023-07-08 17:23:19 +0200

    Add underconstruction image

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,9 +4,16 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import "./Global.css";
 
+import underconstruction from "assets/underconstruction.png";
+
 export const App = () => {
   return (
     <div className="App">
+      <img
+        src={underconstruction}
+        alt="Under construction"
+        style={{ alignSelf: "center", maxWidth: "300px" }}
+      />
       <Outlet />
       <Footer />
     </div>
