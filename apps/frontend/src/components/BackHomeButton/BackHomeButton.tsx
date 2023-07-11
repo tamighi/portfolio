95c6178b0e4e40466866c7c36b78dc8f339ca1f8
tamighi <tamighi@hotmail.com>
2023-07-11 11:36:50 +0200

    BackHomeButton fixed

--- apps/frontend/src/components/BackHomeButton/BackHomeButton.tsx
+++ apps/frontend/src/components/BackHomeButton/BackHomeButton.tsx
@@ -2,23 +2,22 @@ import { Link } from "react-router-dom";
 
 import "./BackHomeButton.css";
 
-type Props = React.HTMLAttributes<HTMLAnchorElement>;
-
-// TODO: Position fixed and disapear when scrolling
-const BackHomeButton = (props: Props) => {
-  const { className, ...rest } = props;
+// TODO: disapear when scrolling
+const BackHomeButton = () => {
   return (
-    <Link className={`BackHomeButton ${className || ""}`} to="/" {...rest}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="HomeIcon"
-      >
-        <path d="M12 4L3 10V21H9V14H15V21H21V10L12 4Z" />
-      </svg>
-      <span>Home</span>
-    </Link>
+    <div className="Header">
+      <Link className="BackHomeButton" to="/">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="HomeIcon"
+        >
+          <path d="M12 4L3 10V21H9V14H15V21H21V10L12 4Z" />
+        </svg>
+        <span>Home</span>
+      </Link>
+    </div>
   );
 };
 
