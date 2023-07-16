7c59659c7e3e82fbb1fc2435def753707cbd6684
thomas <tamighi@hotmail.com>
2023-07-16 08:14:15 +0200

    vite conversion seems ok

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import { App } from "App";
+import { App } from "./App";
 import { ErrorPage, HomePage, ResumePage, WorkPage } from "pages";
 import { PopupProvider } from "providers";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { Root } from "Root";
+import { Root } from "./Root";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
