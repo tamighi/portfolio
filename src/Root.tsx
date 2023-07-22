56384ded3cce28ff8a15026f66cd25dd17ab40d1
tamighi <tamighi@hotmail.com>
2023-07-22 09:34:03 +0200

    Configured basename

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,34 +2,37 @@ import { App } from "./App";
 import { ContactPage, ErrorPage, HomePage, ResumePage, WorkPage } from "pages";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <HomePage />,
-      },
-      {
-        path: "resume",
-        element: <ResumePage />,
-      },
-      {
-        path: "work",
-        element: <WorkPage />,
-      },
-      {
-        path: "contact",
-        element: <ContactPage />,
-      },
-      {
-        path: "*",
-        element: <ErrorPage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "",
+          element: <HomePage />,
+        },
+        {
+          path: "resume",
+          element: <ResumePage />,
+        },
+        {
+          path: "work",
+          element: <WorkPage />,
+        },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "*",
+          element: <ErrorPage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/portfolio/" }
+);
 
 export const Root = () => {
   return <RouterProvider router={router} />;
