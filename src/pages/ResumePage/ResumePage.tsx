c5c47c8a44d118b102ba2ce9e27edfb21523aee0
tamighi <tomi-lys@live.fr>
2024-05-12 10:13:40 +0200

    style(resume): fix alignement

--- src/pages/ResumePage/ResumePage.tsx
+++ src/pages/ResumePage/ResumePage.tsx
@@ -10,15 +10,25 @@ const ResumePage = () => {
       <Appbar />
       <ResponsivePage>
         <div className="flex flex-col gap-2 mb-8">
-          <h2 className="font-bold text-4xl rounded-3xl self-start">About</h2>
+          <h2
+            className="font-bold text-4xl rounded-3xl self-center md:self-start"
+          >
+            About
+          </h2>
           <About />
         </div>
         <div className="flex flex-col gap-2 mb-8">
-          <h2 className="font-bold text-4xl rounded-3xl self-start">Skills</h2>
+          <h2
+            className="font-bold text-4xl rounded-3xl self-center md:self-start"
+          >
+            Skills
+          </h2>
           <Skills />
         </div>
         <div className="flex flex-col gap-2 mb-8">
-          <h2 className="font-bold text-4xl rounded-3xl self-start">
+          <h2
+            className="font-bold text-4xl rounded-3xl self-center md:self-start"
+          >
             Education
           </h2>
           <Education />
