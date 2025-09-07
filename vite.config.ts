282f5e7275f9929e14f02664071f8ede95c62af3
Amighi Thomas <thomas.amighi@hotmail.com>
2025-09-07 09:03:01 +0200

    chore: remove ts-config-path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   base: "/portfolio/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
