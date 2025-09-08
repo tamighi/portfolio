2005cb634adf78baa74c2f8d26c91ffd952f4974
Amighi Thomas <thomas.amighi@hotmail.com>
2025-09-08 11:17:49 +0200

    style: add br br

--- src/pages/ResumePage/About/About.tsx
+++ src/pages/ResumePage/About/About.tsx
@@ -2,7 +2,9 @@ const About = () => {
   return (
     <p>
       Passionate Full-Stack Web Developer with 2 years of experience at the
-      European Commission. <br />
+      European Commission.
+      <br />
+      <br />
       Skilled in modern web technologies, quick to learn, and currently chasing
       my dream of pursuing my career in Stockholm.
     </p>
