4b29494324f0b35062457044d129b80920505639
Amighi Thomas <thomas.amighi@hotmail.com>
2025-09-09 09:46:47 +0200

    fix: phone style

--- src/components/BulletPoints.tsx
+++ src/components/BulletPoints.tsx
@@ -10,7 +10,7 @@ const BulletPoints = ({ bullets, label, textClasses }: Props) => {
       {label && <span className="underline font-bold">{label}</span>}
       <ul className="flex list-disc list-inside space-x-6 flex-wrap">
         {bullets.map((bullet, i) => (
-          <li key={i} className="whitespace-nowrap">
+          <li key={i}>
             <span className={textClasses}>{bullet}</span>
           </li>
         ))}
